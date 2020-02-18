0f59ecef98fbb01153df18d6c7ec629b0c535b5c
siganushka <siganushka@gmail.com>
2020-02-18 14:41:08 +0800

    remove CurrencyUtils && tiwg filter

--- src/Twig/GenericExtension.php
+++ src/Twig/GenericExtension.php
@@ -7,24 +7,11 @@ use Twig\TwigFilter;
 
 class GenericExtension extends AbstractExtension
 {
-    private $currencyUtils;
-
-    public function __construct(CurrencyUtils $currencyUtils)
-    {
-        $this->currencyUtils = $currencyUtils;
-    }
-
     public function getFilters(): array
     {
         return [
-            new TwigFilter('price', [$this, 'priceFilter']),
             new TwigFilter('str_repeat', [$this, 'strRepeat']),
         ];
-    }
-
-    public function priceFilter(int $amount)
-    {
-        return $this->currencyUtils->format($amount);
     }
 
     public function strRepeat(string $input, int $multiplier)
