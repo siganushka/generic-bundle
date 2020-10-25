150b696f5b6d5aa66858e57a782ea2348c7b5c62
siganushka <siganushka@gmail.com>
2020-10-25 21:48:14 +0800

    [feat] 移除模板点位提示

--- src/Form/Type/RegionType.php
+++ src/Form/Type/RegionType.php
@@ -5,17 +5,10 @@ namespace Siganushka\GenericBundle\Form\Type;
 use Siganushka\GenericBundle\Model\Region;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegionType extends AbstractType
 {
-    public function buildView(FormView $view, FormInterface $form, array $options)
-    {
-        $view->vars['attr']['data-placeholder'] = $options['placeholder'];
-    }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
