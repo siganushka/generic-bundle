0b643d836b7628fe6cddba0e87255d7d95504c19
siganushka <siganushka@gmail.com>
2020-10-27 23:06:22 +0800

    [fix] 修复事件对象初始化参数类型

--- src/Event/RegionFilterEvent.php
+++ src/Event/RegionFilterEvent.php
@@ -2,6 +2,7 @@
 
 namespace Siganushka\GenericBundle\Event;
 
+use Siganushka\GenericBundle\Model\RegionInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class RegionFilterEvent extends Event
@@ -13,7 +14,13 @@ class RegionFilterEvent extends Event
 
     public function __construct(array $regions)
     {
-        $this->regions = $regions;
+        foreach ($regions as $region) {
+            if (!$region instanceof RegionInterface) {
+                throw new \InvalidArgumentException(sprintf('Array of regions must be type of %s', RegionInterface::class));
+            }
+
+            array_push($this->regions, $region);
+        }
     }
 
     public function getRegions(): array
