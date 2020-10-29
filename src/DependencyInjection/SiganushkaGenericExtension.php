97f4abc40a0b6904a8893c0ec0b73dbcd413f355
siganushka <siganushka@gmail.com>
2020-10-29 23:52:11 +0800

    [fix] 配置文件由 PHP 改为 xml 以兼容老版本

--- src/DependencyInjection/SiganushkaGenericExtension.php
+++ src/DependencyInjection/SiganushkaGenericExtension.php
@@ -7,15 +7,15 @@ use Siganushka\GenericBundle\EventSubscriber\JsonResponseSubscriber;
 use Siganushka\GenericBundle\Form\Extension\DisableHtml5ValidateTypeExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SiganushkaGenericExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new PhpFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
-        $loader->load('services.php');
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.xml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
