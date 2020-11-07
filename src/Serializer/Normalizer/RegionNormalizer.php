d04b3dc60dd35d3db88da878996f3df8ceee2212
siganushka <siganushka@gmail.com>
2020-11-07 22:30:55 +0800

    [feat] 新增行政区划序列划器

---
+++ src/Serializer/Normalizer/RegionNormalizer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Siganushka\GenericBundle\Serializer\Normalizer;
+
+use Siganushka\GenericBundle\Model\RegionInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class RegionNormalizer implements NormalizerInterface
+{
+    public function normalize($object, $format = null, array $context = [])
+    {
+        return [
+            'code' => $object->getCode(),
+            'name' => $object->getName(),
+        ];
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof RegionInterface;
+    }
+}
