2f175a97dbb0d143d5e7c8b496818d9cf8fad74d
siganushka <siganushka@gmail.com>
2022-10-26 17:59:47 +0800

    refactor: improving SequenceGenerator && tests

--- src/Identifier/SequenceGenerator.php
+++ src/Identifier/SequenceGenerator.php
@@ -10,32 +10,31 @@ namespace Siganushka\GenericBundle\Identifier;
 class SequenceGenerator
 {
     /**
-     * 生成序列编号，单机下理论重复概率为百万分之一秒，保证最小长度和唯一性。
+     * 生成序列号，单机下理论重复概率为百万分之一秒，保证最小长度和碰撞概率。
      *
      * 生成特征：
-     *
-     * - 纯数字
-     * - 固定长度（16 位）
-     * - 有先后顺序（可排序）
-     * - 更直观的体现时间范围（可反向解开对应时间）
-     * - 非自增长（避免被采集或暴露业务量）
+     *  纯数字
+     *  固定长度（16 位）
+     *  有先后顺序（可排序）
+     *  非自增长（避免被采集或暴露业务量）
+     *  直观的体现时间范围（可反向解开对应时间）
      *
      * 生成规律：
-     *
-     * 1-2 位：年份，比如 19 代表 2019 年
-     * 3-5 位：一年中的第几天，比如 016 就是一年中的第 16 天，最大值为 365
-     * 6-10 位：第一天中的秒数，从 0 开始，最大 86400
-     * 11-16 位：当前系统时间的微秒数（百万分之一秒）
+     *  1-2 位：年份，比如 19 代表 2019 年
+     *  3-5 位：当年中的第几天，比如 016 就是一年中的第 16 天，最大值为 365
+     *  6-10 位：当天已过的秒数，从 0 天，最大值为 86400
+     *  11-16 位：当前系统时间的微秒数（百万分之一秒）
      */
     public function generate(): string
     {
-        $current = new \DateTime();
-        $midnight = new \DateTime('midnight');
+        $now = new \DateTimeImmutable();
+        /** @var \DateTimeImmutable */
+        $today = $now->modify('today');
 
-        $y = $current->format('y');
-        $z = $current->format('z');
-        $s = $current->getTimestamp() - $midnight->getTimestamp();
-        $u = $current->format('u');
+        $y = $now->format('y');
+        $z = $now->format('z');
+        $s = $now->getTimestamp() - $today->getTimestamp();
+        $u = $now->format('u');
 
         return sprintf('%02s%03s%05s%06s', $y, $z, $s, $u);
     }
--- tests/Identifier/SequenceGeneratorTest.php
+++ tests/Identifier/SequenceGeneratorTest.php
@@ -16,5 +16,41 @@ class SequenceGeneratorTest extends TestCase
 
         static::assertNotEmpty($identifier);
         static::assertSame(16, mb_strlen($identifier));
+
+        $y = mb_substr($identifier, 0, 2);
+        $z = mb_substr($identifier, 2, 3);
+        $s = mb_substr($identifier, 5, 5);
+
+        $now = new \DateTimeImmutable();
+        $today = $now->modify('today');
+
+        static::assertSame($y, $now->format('y'));
+        static::assertSame($z, $now->format('z'));
+        static::assertSame($s, (string) ($now->getTimestamp() - $today->getTimestamp()));
     }
+
+    // public function testPerformance(): void
+    // {
+    //     $generator = new SequenceGenerator();
+    //     $time_pre = microtime(true);
+
+    //     $count = 1000000;
+    //     $error = 0;
+
+    //     $values = [];
+    //     for ($i = 0; $i < $count; ++$i) {
+    //         $v = $generator->generate();
+    //         if (isset($values[$v])) {
+    //             $error ++;
+    //             continue;
+    //         }
+
+    //         $values[$v] = true;
+    //     }
+
+    //     $time_post = microtime(true);
+    //     $exec_time = $time_post - $time_pre;
+
+    //     dd(sprintf('生成 %d，成功 %d，重复 %d，耗时 %s', $count, count($values), $error, $exec_time));
+    // }
 }
