c03e52029f6b59a0cb8dca53224e1f39a1d6272f
siganushka <siganushka@gmail.com>
2023-08-09 21:11:19 +0800

    feat: add GenericEntityRepository

---
+++ src/Repository/GenericEntityRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Siganushka\Contracts\Doctrine\ResourceInterface;
+use Siganushka\Contracts\Doctrine\SortableInterface;
+use Siganushka\Contracts\Doctrine\TimestampableInterface;
+
+class GenericEntityRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, string $entityClass)
+    {
+        if (!is_subclass_of($entityClass, ResourceInterface::class)) {
+            throw new \UnexpectedValueException(sprintf('Expected argument of type "%s", "%s" given', ResourceInterface::class, $entityClass));
+        }
+
+        parent::__construct($registry, $entityClass);
+    }
+
+    /**
+     * @param string      $alias   the alias for the table
+     * @param string|null $indexBy the index for the from
+     */
+    public function createQueryBuilder($alias, $indexBy = null): QueryBuilder
+    {
+        $queryBuilder = parent::createQueryBuilder($alias, $indexBy)
+            ->orderBy(sprintf('%s.id', $alias), 'DESC')
+        ;
+
+        if (is_subclass_of($this->_entityName, SortableInterface::class)) {
+            $queryBuilder->addOrderBy(sprintf('%s.sorted', $alias), 'DESC');
+        }
+
+        if (is_subclass_of($this->_entityName, TimestampableInterface::class)) {
+            $queryBuilder->addOrderBy(sprintf('%s.createdAt', $alias), 'DESC');
+        }
+
+        return $queryBuilder;
+    }
+
+    /**
+     * @param mixed ...$args
+     */
+    public function createNew(...$args): ResourceInterface
+    {
+        $ref = new \ReflectionClass($this->_entityName);
+
+        /** @var ResourceInterface */
+        $entity = \count($args)
+            ? $ref->newInstance(...$args)
+            : $ref->newInstanceWithoutConstructor();
+
+        return $entity;
+    }
+}
---
+++ tests/Repository/GenericEntityRepositoryTest.php
@@ -0,0 +1,125 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Tests\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepositoryInterface;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use PHPUnit\Framework\TestCase;
+use Siganushka\Contracts\Doctrine\ResourceInterface;
+use Siganushka\Contracts\Doctrine\ResourceTrait;
+use Siganushka\Contracts\Doctrine\SortableInterface;
+use Siganushka\Contracts\Doctrine\SortableTrait;
+use Siganushka\Contracts\Doctrine\TimestampableInterface;
+use Siganushka\Contracts\Doctrine\TimestampableTrait;
+use Siganushka\GenericBundle\Repository\GenericEntityRepository;
+
+class GenericEntityRepositoryTest extends TestCase
+{
+    public function testAll(): void
+    {
+        $repository = $this->createRepository(Foo::class);
+        static::assertInstanceOf(ServiceEntityRepositoryInterface::class, $repository);
+
+        /** @var Foo */
+        $entity = $repository->createNew();
+        static::assertInstanceOf(Foo::class, $entity);
+        static::assertNull($entity->getArg1());
+        static::assertNull($entity->getArg2());
+
+        /** @var Foo */
+        $entity = $repository->createNew('hello', 256);
+        static::assertInstanceOf(Foo::class, $entity);
+        static::assertSame('hello', $entity->getArg1());
+        static::assertSame(256, $entity->getArg2());
+
+        $entity->setArg1('world');
+        $entity->setArg2(512);
+        static::assertSame('world', $entity->getArg1());
+        static::assertSame(512, $entity->getArg2());
+
+        static::assertSame(
+            'SELECT f FROM Siganushka\GenericBundle\Tests\Repository\Foo f ORDER BY f.id DESC, f.sorted DESC, f.createdAt DESC',
+            $repository->createQueryBuilder('f')->getDQL()
+        );
+    }
+
+    public function testUnexpectedValueException(): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+
+        $this->createRepository(Bar::class);
+    }
+
+    private function createRepository(string $entityClass): GenericEntityRepository
+    {
+        $classMetadata = new ClassMetadata($entityClass);
+
+        $entityManager = $this->createMock(EntityManager::class);
+        $entityManager->expects(static::any())
+            ->method('getClassMetadata')
+            ->willReturn($classMetadata)
+        ;
+
+        $entityManager->expects(static::any())
+            ->method('createQueryBuilder')
+            ->willReturn(new QueryBuilder($entityManager))
+        ;
+
+        $managerRegistry = $this->createMock(ManagerRegistry::class);
+        $managerRegistry->expects(static::any())
+            ->method('getManagerForClass')
+            ->willReturn($entityManager)
+        ;
+
+        return new GenericEntityRepository($managerRegistry, $entityClass);
+    }
+}
+
+class Foo implements ResourceInterface, SortableInterface, TimestampableInterface
+{
+    use ResourceTrait;
+    use SortableTrait;
+    use TimestampableTrait;
+
+    private ?string $arg1 = null;
+    private ?int $arg2 = null;
+
+    public function __construct(string $arg1, int $arg2 = 128)
+    {
+        $this->arg1 = $arg1;
+        $this->arg2 = $arg2;
+    }
+
+    public function setArg1(string $arg1): self
+    {
+        $this->arg1 = $arg1;
+
+        return $this;
+    }
+
+    public function getArg1(): ?string
+    {
+        return $this->arg1;
+    }
+
+    public function setArg2(int $arg2): self
+    {
+        $this->arg2 = $arg2;
+
+        return $this;
+    }
+
+    public function getArg2(): ?int
+    {
+        return $this->arg2;
+    }
+}
+
+class Bar
+{
+}
