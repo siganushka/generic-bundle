c28940f1f1c236c07452ca057454106480483fad
siganushka <siganushka@gmail.com>
2024-01-08 12:09:18 +0800

    refactor: remove GenericSortedNormalizerTest

--- src/Resources/config/serializer.php
+++ src/Resources/config/serializer.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Siganushka\GenericBundle\Serializer\Normalizer\GenericSortedNormalizer;
 use Siganushka\GenericBundle\Serializer\Normalizer\KnpPaginationNormalizer;
 use Siganushka\GenericBundle\Serializer\Normalizer\TranslatableNormalizer;
 
 return static function (ContainerConfigurator $container): void {
     $container->services()
-        ->set('siganushka_generic.serializer.normalizer.generic_sorted', GenericSortedNormalizer::class)
-            ->arg(0, service('serializer.normalizer.object'))
-            ->tag('serializer.normalizer')
-
         ->set('siganushka_generic.serializer.normalizer.knp_pagination', KnpPaginationNormalizer::class)
             ->tag('serializer.normalizer')
 
--- src/SiganushkaGenericBundle.php
+++ src/SiganushkaGenericBundle.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace Siganushka\GenericBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SiganushkaGenericBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
-    {
-    }
 }
