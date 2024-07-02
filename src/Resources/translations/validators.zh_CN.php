2ec8853ad48fb541ba074424f988678fa201ea97
siganushka <siganushka@gmail.com>
2024-07-02 16:26:47 +0800

    refactor: Using php format replace yaml for translations

---
+++ src/Resources/translations/validators.zh_CN.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Validation Failed.' => '表单验证失败。',
+    'This value is not a valid phone number.' => '手机号码格式无效。',
+    'This value is not a valid semantic version.' => '版本号格式无效。',
+];
