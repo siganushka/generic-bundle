564c7f4493c7cca26a8377a628ea494809e4b6f1
siganushka <siganushka@gmail.com>
2024-12-09 11:11:14 +0800

    fix: The Chinese/English translation message is written in reverse

--- translations/validators.en.php
+++ translations/validators.en.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'Validation Failed.' => '表单验证失败。',
-    'This value is not a valid phone number.' => '手机号码格式无效。',
-    'This value is not a valid semantic version.' => '版本号格式无效。',
+    'Validation Failed.' => 'Validation Failed.',
+    'This value is not a valid phone number.' => 'This value is not a valid phone number.',
+    'This value is not a valid semantic version.' => 'This value is not a valid semantic version.',
 ];
