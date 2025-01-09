c99e1f32a041ef503af20cf91b9729487d9f854d
siganushka <siganushka@gmail.com>
2025-01-09 22:02:06 +0800

    feat: add translatable for Gender enum

--- src/Enum/Gender.php
+++ src/Enum/Gender.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Siganushka\GenericBundle\Enum;
 
-enum Gender: string
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum Gender: string implements TranslatableInterface
 {
     case Male = 'M';
     case Female = 'F';
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return match ($this) {
+            self::Male => $translator->trans('generic.gender_male', locale: $locale),
+            self::Female => $translator->trans('generic.gender_female', locale: $locale),
+        };
+    }
 }
