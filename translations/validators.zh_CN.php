de9cc3741ddb983b0b94b4262e2fdcf0a3b5fe6b
siganushka <siganushka@gmail.com>
2025-07-24 15:45:44 +0800

    refactor: update validation trans message

--- translations/validators.zh_CN.php
+++ translations/validators.zh_CN.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'This value is not a valid phone number.' => '手机号码格式无效。',
-    'This value is not a valid semantic version.' => '版本号格式无效。',
+    'This value is not a valid phone number.' => '该变量不是一个有效的手机号码。',
+    'This value is not a valid semantic version.' => '该变量不是一个有效的版本号。',
 ];
