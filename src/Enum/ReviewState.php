99fc6eb99282bb81c92781fc93f675fc0a86b886
siganushka <siganushka@gmail.com>
2025-08-06 15:56:17 +0800

    fix: fixed error namespace

--- src/Enum/ReviewState.php
+++ src/Enum/ReviewState.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Enum;
+namespace Siganushka\GenericBundle\Enum;
 
 use Symfony\Contracts\Translation\TranslatableInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
