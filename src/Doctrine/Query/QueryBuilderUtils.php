2764574ec98df3a11b65457f2b3551bb7bdbbd66
siganushka <siganushka@gmail.com>
2025-10-10 17:23:45 +0800

    feat: add QueryBuilderUtils

---
+++ src/Doctrine/Query/QueryBuilderUtils.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Doctrine\Query;
+
+use Doctrine\ORM\QueryBuilder;
+
+class QueryBuilderUtils
+{
+    public static function addDateRangeFilter(QueryBuilder $queryBuilder, ?\DateTimeInterface $startAt, ?\DateTimeInterface $endAt): QueryBuilder
+    {
+        $alias = $queryBuilder->getAllAliases()[0];
+
+        $startAt && $queryBuilder->andWhere(\sprintf('%s.createdAt >= :startAt', $alias))->setParameter('startAt', $startAt);
+        $endAt && $queryBuilder->andWhere(\sprintf('%s.createdAt <= :endAt', $alias))->setParameter('endAt', $endAt);
+
+        return $queryBuilder;
+    }
+
+    public static function addEnabledFilter(QueryBuilder $queryBuilder, ?bool $enabled): QueryBuilder
+    {
+        $alias = $queryBuilder->getAllAliases()[0];
+
+        if (\is_bool($enabled)) {
+            $queryBuilder->andWhere(\sprintf('%s.enabled = :enabled', $alias))->setParameter('enabled', $enabled);
+        }
+
+        return $queryBuilder;
+    }
+}
---
+++ tests/Doctrine/Query/QueryBuilderUtilsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Tests\Doctrine\Filter;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use PHPUnit\Framework\TestCase;
+use Siganushka\GenericBundle\Doctrine\Query\QueryBuilderUtils;
+use Siganushka\GenericBundle\Tests\Fixtures\Foo;
+
+class QueryBuilderUtilsTest extends TestCase
+{
+    public function testAll(): void
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+
+        $queryBuilder = new QueryBuilder($entityManager);
+        $queryBuilder->from(Foo::class, 'f')->select('f');
+
+        QueryBuilderUtils::addDateRangeFilter($queryBuilder, null, null);
+        static::assertSame('SELECT f FROM Siganushka\GenericBundle\Tests\Fixtures\Foo f', $queryBuilder->getDQL());
+
+        $now = new \DateTime();
+        QueryBuilderUtils::addDateRangeFilter($queryBuilder, $now, null);
+        static::assertSame('SELECT f FROM Siganushka\GenericBundle\Tests\Fixtures\Foo f WHERE f.createdAt >= :startAt', $queryBuilder->getDQL());
+
+        QueryBuilderUtils::addDateRangeFilter($queryBuilder, null, $now);
+        static::assertSame('SELECT f FROM Siganushka\GenericBundle\Tests\Fixtures\Foo f WHERE f.createdAt >= :startAt AND f.createdAt <= :endAt', $queryBuilder->getDQL());
+
+        // Reseting DQL where part.
+        $queryBuilder->resetDQLPart('where');
+
+        QueryBuilderUtils::addEnabledFilter($queryBuilder, null);
+        static::assertSame('SELECT f FROM Siganushka\GenericBundle\Tests\Fixtures\Foo f', $queryBuilder->getDQL());
+
+        QueryBuilderUtils::addEnabledFilter($queryBuilder, true);
+        static::assertSame('SELECT f FROM Siganushka\GenericBundle\Tests\Fixtures\Foo f WHERE f.enabled = :enabled', $queryBuilder->getDQL());
+    }
+}
