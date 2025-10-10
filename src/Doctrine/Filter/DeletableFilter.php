a39464b52442272c3a5bf5d6ef847a61cad09e90
siganushka <siganushka@gmail.com>
2025-10-10 17:23:34 +0800

    tests: add DeletableFilterTest

--- src/Doctrine/Filter/DeletableFilter.php
+++ src/Doctrine/Filter/DeletableFilter.php
@@ -10,11 +10,11 @@ use Siganushka\Contracts\Doctrine\DeletableInterface;
 
 class DeletableFilter extends SQLFilter
 {
+    /**
+     * @see https://www.doctrine-project.org/projects/doctrine-orm/en/3.5/reference/filters.html
+     */
     public function addFilterConstraint(ClassMetadata $targetEntity, string $targetTableAlias): string
     {
-        /*
-         * @see https://www.doctrine-project.org/projects/doctrine-orm/en/3.5/reference/filters.html
-         */
         if ($targetEntity->reflClass?->implementsInterface(DeletableInterface::class)) {
             return \sprintf('%s.deleted_at IS NULL', $targetTableAlias);
         }
---
+++ tests/Doctrine/Filter/DeletableFilterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Tests\Doctrine\Filter;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use PHPUnit\Framework\TestCase;
+use Siganushka\GenericBundle\Doctrine\Filter\DeletableFilter;
+
+class DeletableFilterTest extends TestCase
+{
+    public function testAll(): void
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+
+        $reflClass = $this->createMock(\ReflectionClass::class);
+        $reflClass->expects(static::any())
+            ->method('implementsInterface')
+            ->willReturn(true)
+        ;
+
+        $foo = $this->createMock(ClassMetadata::class);
+        $bar = $this->createMock(ClassMetadata::class);
+        $bar->reflClass = $reflClass;
+
+        $filter = new DeletableFilter($entityManager);
+        static::assertSame('', $filter->addFilterConstraint($foo, 'foo'));
+        static::assertSame('bar.deleted_at IS NULL', $filter->addFilterConstraint($bar, 'bar'));
+    }
+}
