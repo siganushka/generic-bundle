64462a0ba09fce51d4cf4e762fc202f878e7e6be
siganushka <siganushka@gmail.com>
2025-11-10 09:55:02 +0800

    refactor: remove FormTypeExtension

--- config/form.php
+++ config/form.php
@@ -8,7 +8,6 @@ use Siganushka\GenericBundle\Controller\FormController;
 use Siganushka\GenericBundle\Form\Extension\ButtonTypeExtension;
 use Siganushka\GenericBundle\Form\Extension\ChoiceTypeExtension;
 use Siganushka\GenericBundle\Form\Extension\CollectionTypeExtension;
-use Siganushka\GenericBundle\Form\Extension\FormTypeExtension;
 use Siganushka\GenericBundle\Form\Extension\MoneyTypeExtension;
 
 return static function (ContainerConfigurator $container): void {
@@ -16,9 +15,6 @@ return static function (ContainerConfigurator $container): void {
         ->set('siganushka_generic.form.controller', FormController::class)
             ->arg(0, tagged_iterator('form.type'))
             ->tag('controller.service_arguments')
-
-        ->set('siganushka_generic.form.form_type_extension', FormTypeExtension::class)
-            ->tag('form.type_extension')
 
         ->set('siganushka_generic.form.money_type_extension', MoneyTypeExtension::class)
             ->tag('form.type_extension')
