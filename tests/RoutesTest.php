768adab8f61b8286d2cad1d6a285365a7ae209ab
siganushka <siganushka@gmail.com>
2025-12-05 14:19:03 +0800

    tests: add RoutesTest

---
+++ tests/RoutesTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\GenericBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\PhpFileLoader;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+class RoutesTest extends TestCase
+{
+    protected RouteCollection $routes;
+
+    protected function setUp(): void
+    {
+        $loader = new PhpFileLoader(new FileLocator(__DIR__.'/../config/'));
+        $this->routes = $loader->load('routes.php');
+    }
+
+    public function testAll(): void
+    {
+        $routes = iterator_to_array(self::routesProvider());
+        $routeNames = array_map(fn (array $route) => $route[0], $routes);
+
+        static::assertSame($routeNames, array_keys($this->routes->all()));
+    }
+
+    /**
+     * @dataProvider routesProvider
+     */
+    public function testRotues(string $routeName, string $path, array $methods, string $controller): void
+    {
+        /** @var Route */
+        $route = $this->routes->get($routeName);
+
+        static::assertSame($path, $route->getPath());
+        static::assertSame($methods, $route->getMethods());
+        static::assertSame($controller, $route->getDefault('_controller'));
+    }
+
+    public static function routesProvider(): iterable
+    {
+        yield ['siganushka_generic_form', '/_form', [], 'siganushka_generic.form.controller'];
+    }
+}
