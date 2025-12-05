091a18121203b8a6ce1696e55f8cc9f8f46ed788
siganushka <siganushka@gmail.com>
2025-12-05 13:31:38 +0800

    refactor: add missing trans message for CnNameValidator

--- translations/validators.en.php
+++ translations/validators.en.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 return [
     'Validation Failed' => 'Validation Failed',
+    'This value is not a valid chinese name.' => 'This value is not a valid chinese name.',
     'This value is not a valid phone number.' => 'This value is not a valid phone number.',
     'This value is not a valid semantic version.' => 'This value is not a valid semantic version.',
     'This value should be less than or equal to {{ formatted_value }}.' => 'This value should be less than or equal to {{ formatted_value }}.',
