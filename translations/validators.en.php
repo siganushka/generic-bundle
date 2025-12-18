5d5fe63059c269419708d5f2b12ee189367a014a
siganushka <siganushka@gmail.com>
2025-12-18 16:26:40 +0800

    refactor: improve trans messages

--- translations/validators.en.php
+++ translations/validators.en.php
@@ -7,5 +7,4 @@ return [
     'This value is not a valid chinese name.' => 'This value is not a valid chinese name.',
     'This value is not a valid phone number.' => 'This value is not a valid phone number.',
     'This value is not a valid semantic version.' => 'This value is not a valid semantic version.',
-    'This value should be less than or equal to {{ formatted_value }}.' => 'This value should be less than or equal to {{ formatted_value }}.',
 ];
--- translations/validators.zh_CN.php
+++ translations/validators.zh_CN.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'Validation Failed' => '数据验证失败。',
+    'Validation Failed' => '验证失败。',
     'This value is not a valid chinese name.' => '该值不是一个有效的中文姓名。',
     'This value is not a valid phone number.' => '该值不是一个有效的手机号码。',
     'This value is not a valid semantic version.' => '该值不是一个有效的版本号。',
