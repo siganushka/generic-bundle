336260ee5a647a3c31aab7fdb3fd1537045c3ea2
siganushka <siganushka@gmail.com>
2026-03-13 14:41:38 +0800

    refactor: remove JsonResponseListener for automatic json encoding options

--- config/services.php
+++ config/services.php
@@ -5,15 +5,11 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Siganushka\GenericBundle\Decorator\DecoratingKnpPaginator;
-use Siganushka\GenericBundle\EventListener\JsonResponseListener;
 use Siganushka\GenericBundle\Twig\Extension\GenericExtension;
 use Siganushka\GenericBundle\Twig\Runtime\GenericExtensionRuntime;
 
 return static function (ContainerConfigurator $container): void {
     $container->services()
-        ->set('siganushka_generic.json_response_listener', JsonResponseListener::class)
-            ->tag('kernel.event_subscriber')
-
         ->set('siganushka_generic.knp_paginator_decorator', DecoratingKnpPaginator::class)
             ->arg('$decorated', service('siganushka_generic.knp_paginator_decorator.inner'))
             ->arg('$requestStack', service('request_stack'))
