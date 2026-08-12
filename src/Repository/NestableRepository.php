13b15bc29d7904790fc46eeb68cdaa13ea89d0c7
siganushka <siganushka@gmail.com>
2026-08-12 11:47:03 +0800

    refactor: improve NestableRepository generic types

--- src/Repository/NestableRepository.php
+++ src/Repository/NestableRepository.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Siganushka\GenericBundle\Repository;
 
-use Siganushka\GenericBundle\Entity\AbstractNestable;
+use Siganushka\GenericBundle\Model\NestableInterface;
 
 /**
- * @template T of AbstractNestable = AbstractNestable
+ * @template TNode of NestableInterface = NestableInterface
  *
- * @extends GenericEntityRepository<T>
+ * @extends GenericEntityRepository<TNode>
  */
 class NestableRepository extends GenericEntityRepository
 {
     /**
-     * @return array<int, T>
+     * @return list<TNode>
      */
     public function findByParent(int|string|null $parent, ?array $orderBy = null, ?int $limit = null, ?int $offset = null): array
     {
